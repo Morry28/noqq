59c01528ca2cfad1e90a3398e57a7db0325664c5
Roman Janek <romanjanek.yt@gmail.com>
2025-03-16 02:37:11 +0100

    Removed dist/ and node_modules/ from Git tracking

--- dist/utils/prompts/prompts.d.ts
+++
@@ -1,5 +0,0 @@
-export declare const arrayPrompt: (sample: any, prompt: string, decomposition: string) => string;
-export declare const streamingPrompt: (sample: any, prompt: string, decomposition: string) => string;
-export declare const arrayPromptFintech: (sample: any, prompt: string, decomposition: string) => string;
-export declare const validatePrompt: (func: string, prompt: string, sample: any) => string;
-export declare const decomposePrompt: (sample: any, prompt: any) => string;
